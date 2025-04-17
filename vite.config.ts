a70f8d5b49068894112f5200f5b3f28937208e8c
Edu Souza <eduecrissouza@gmail.com>
2025-04-17 18:03:22 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic' // Adicione esta linha se usar React 16+
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
   server: {
-    host: '0.0.0.0',
-    port: 5173,
+    host: true,
+    port: 5174,
     strictPort: true,
-    open: true
+    open: true,
+    cors: true
   },
   build: {
     outDir: 'dist',
